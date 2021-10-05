788a87be8badb4508f9ba250e7ca486b8713f23c
YmmSgy <briansujl@gmail.com>
2021-10-05 21:34:45 +0800

    Expanded on objects and their members
    Buffer obj: Added settile() method.
    Buffer obj: Moved tiles array parsing from individual methods to the flush() method. This is so methods like fill() and settile() don't have to deal with newlines and string concatenation. Also so methods can use mutable arrays instead of immutable strings.
    Created cd (2d-coordinates) object prototype.
    Created snake object which stores the positions of every body segment, and the current moving direction, and a move() method to be called every turn. isInBody() checks if a coordinate lies within the snake's body.
    Made button to step through turns. Commented out the real-time turn-based system until testing is complete.

--- snake.js
+++ snake.js
@@ -1,36 +1,82 @@
+function Cd(x, y) {
+    this.x = x;
+    this.y = y;
+    this.toMonoCd = () => 10 * this.y + x;
+    // vector addition
+    this.add = (other) => new Cd(this.x + other.x, this.y + other.y);
+}
+
 const screen = document.getElementById("screen");
-const initxy = {x:4, y:4};
+const initxy = new Cd(4, 4);
+
 const buffer = {
-    tiles: "",
+    tiles: [],
 
     // flush buffer
     flush: function() {
-        screen.innerHMTL = this.tiles;
+        let src = this.tiles;
+        let arr = [];
+        // group 10 chars of src, then insert newline
+        for (let i = 0; i < src.length; i += 10) {
+            // slice() returns a sub-array, join() converts to string, add this to arr
+            arr.push(src.slice(i, i + 10).join(""));
+            // add newline to arr
+            arr.push("\n");
+        }
+        // join into full string, slicing out unnecessary newline at the end
+        screen.innerHTML = arr.slice(0, arr.length - 1).join("");
     },
 
     // clear and fill buffer
     fill: function(tile) {
-        for (let y = 0; y < 10; ++y) {
-            for (let x = 0; x < 10; ++x) {
-                this.tiles += tile;
-            }
-            this.tiles += "\n";
+        this.tiles = [];
+        for (let i = 0; i < 100; ++i) {
+            this.tiles.push(tile);
         }
     },
 
-    // count number of tiles
-    count: function() {
-        return this.tiles.length;
+    // set tile in buffer
+    settile: function(cd, tile) {
+        this.tiles[cd.toMonoCd()] = tile;
     }
 };
 
+const snake = {
+    body: [initxy, initxy],
+
+    dir: new Cd(0, 1),
+
+    isInBody: function(cd) {
+        let flag = false;
+        for (let i = 0; i < this.body.length; ++i) {
+            const bodypart = this.body[i];
+            if (bodypart.x === cd.x && bodypart.y === cd.y) { flag = true; }
+        }
+        return flag;
+    },
+
+    move: function() {
+        // remove the tail (index 0) and add prevhead+dir to the head (index length-1)
+        const prevtail = this.body.shift();
+        const prevhead = this.body[this.body.length - 1];
+        const newhead = prevhead.add(this.dir);
+        this.body.push(newhead);
+
+        // clear screen and redraw whole snake
+        buffer.fill(".");
+        this.body.forEach((partcd) => buffer.settile(partcd, "O"));
+        buffer.flush();
+
+        console.log(this.body);
+    }
+};
 
 function turnactions() {
-    
+    // move the snake
+    snake.move();
 }
 
-
 buffer.fill(".");
+buffer.settile(initxy, "O");
 buffer.flush();
-alert(buffer.count());
-setInterval(turnactions, 1000);
+document.getElementById("btn").onclick = turnactions;
