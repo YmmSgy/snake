5fbf330ffddcfd3db0daafe8314f132acdaa74de
YmmSgy <briansujl@gmail.com>
2021-10-10 17:55:59 +0800

    Moved tile painting algorithms into tiles.js

---
+++ tiles.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const screen = document.getElementById("screen");
+const ctx = screen.getContext("2d");
+
+// paints background
+function paintBackground(cd) {
+    ctx.fillStyle = "#000000";
+    const w = Buffer.main.tilewidth;
+
+    ctx.fillRect(cd.x * w, cd.y * w, w, w);
+}
+
+// paints snake body
+function paintBody(cd) {
+    paintBackground(cd);
+    ctx.fillStyle = "#ff0000";
+    const w = Buffer.main.tilewidth;
+
+    ctx.beginPath();
+    ctx.arc(cd.x * w + w / 2, cd.y * w + w / 2, w / 2, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+// paints food
+function paintFood(cd) {
+    paintBackground(cd);
+    ctx.fillStyle = "#00ff00";
+    const w = Buffer.main.tilewidth;
+
+    ctx.beginPath();
+    ctx.arc(cd.x * w + w / 2, cd.y * w + w / 2, w / 2, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+function redraw() {
+    // clear screen
+    Buffer.main.fill(paintBackground);
+
+    // redraw whole snake
+    snake.body.forEach(function(partcd) {
+        Buffer.main.settile(partcd, paintBody);
+    });
+
+    // redraw food
+    Buffer.main.settile(food.pos, paintFood);
+
+    // flush buffer
+    Buffer.main.flush();
+}
